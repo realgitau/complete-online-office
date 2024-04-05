2f9c74f9efc2b49d4a3b9ae80ef254d5bc6dd7d2
realgitau <antonygitau04@gmail.com>
2024-04-05 20:36:19 +0300

    12th commit

--- components/Books/Books.tsx
+++ components/Books/Books.tsx
@@ -1,6 +1,42 @@
 import React from 'react';
 
+// A component for displaying individual book information
+const Book = ({ title, imageSrc, price, description }) => {
+  return (
+    <div className="relative overflow-hidden bg-white shadow-md rounded-xl max-w-md">
+      <img src={imageSrc} alt={title} className="h-70 w-full object-cover rounded-xl" />
+      <div className="p-4">
+        <h3 className="text-xl md:text-2xl font-bold text-gray-900">{title}</h3>
+        <p className="mt-2 text-base text-gray-600">{description}</p>
+        <p className="mt-2 text-base text-gray-600">Price: {price}</p>
+      </div>
+    </div>
+  );
+};
+
 const Books = () => {
+  const books = [
+    {
+      title: "Diary of the Miaha Memoir",
+      imageSrc: "/verahprofile.jpeg",
+      price: "Ksh. 950, AVAILABLE AT Nuria store.",
+      description: "This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband's. It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It's about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks."
+    },
+    {
+      title: "Diary of the Miaha Memoir",
+      imageSrc: "/verahprofile.jpeg",
+      price: "Ksh. 950, AVAILABLE AT Nuria store.",
+      description: "This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband's. It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It's about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks."
+    },
+    {
+      title: "Diary of the Miaha Memoir",
+      imageSrc: "/verahprofile.jpeg",
+      price: "Ksh. 950, AVAILABLE AT Nuria store.",
+      description: "This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband's. It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It's about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks."
+    },
+    // Add more book entries here
+  ];
+
   return (
     <section className="py-12 bg-white text-black sm:py-12 lg:py-16 p-10">
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -9,93 +45,15 @@ const Books = () => {
           <p className="mb-4">Here are some of the Awards that I&apos;ve received over the years</p>
         </div>
         <div className="grid max-w-4xl lg:max-w-6xl grid-cols-1 mx-auto mt-8 text-center gap-y-4 sm:gap-x-8 sm:grid-cols-2 lg:grid-cols-3 sm:text-left">
-          <div className="relative">
-            <div className="relative overflow-hidden bg-white shadow-md rounded-xl h-full">
-              <img src="/verahprofile.jpeg" alt="Image 1" className="h-70 w-full object-cover rounded-xl" />
-              <div className="p-1">
-                <h3 className="mt-1 text-2xl font-bold text-gray-900 sm:mt-10">Diary of the Miaha Memoir</h3>
-                <div className="mt-1 text-base text-gray-600">
-                  <p>
-                    Ksh. 950, AVAILABLE AT Nuria store.
-                    This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband's.
-                    It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It's about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="overflow-hidden bg-white shadow-md rounded-xl max-w-md">
-            <div className="p-4">
-            <img src="/verahprofile.jpeg" alt="Image 1" className="h-70 w-full object-cover rounded-xl" />
-              <div className="mt-2">
-                <h3 className="text-xl md:text-2xl font-bold text-gray-900">The Crescent Moon Award-winning short story</h3>
-                <p className="mt-2 text-base text-gray-600">
-                  Ksh. 300. Available at Nuria.
-                </p>
-                <p className="mt-2 text-base text-gray-600">
-                  'The Crescent Moon' is a short story that won the 2017 Tito Livio Award for Historical Fiction organized by the Italian Institute of Culture in Nairobi and Sugarpulp, Italy. It recounts the tumultuous events of Kenya's 1992 election violence.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative">
-            <div className="relative overflow-hidden bg-white shadow-md rounded-xl h-full">
-            <img src="/verahprofile.jpeg" alt="Image 1" className="h-70 w-full object-cover rounded-xl" />
-              <div className="p-4">
-                <h3 className="mt-2 text-2xl font-bold text-gray-900 sm:mt-10">The Craft of Creative Writing with Mugeni Ojiambo Ksh. 1,000</h3>
-                <p className="mt-2 text-base text-gray-600">
-                  Have you been fantasizing about writing a great book? Or you&apos;ve written, and all you get are rejections? The Craft of Creative Writing is the writing companion you need.
-                  No matter what your writing goals are, this book ensures nothing stands between you and good writing. It is a handbook. A guide. A reference point. A promise of hope for your writing. Exactly what you need. Writing is work, so let&apos;s get to work, shall
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative">
-            <div className="relative overflow-hidden bg-white shadow-md rounded-xl h-full">
-              <img src="/verahprofile.jpeg" alt="Image 1" className="h-70 w-full object-cover rounded-xl" />
-              <div className="p-4">
-                <h3 className="mt-2 text-2xl font-bold text-gray-900 sm:mt-10">The Craft of Creative Writing with Mugeni Ojiambo Ksh. 1,000</h3>
-                <p className="mt-2 text-base text-gray-600">
-                  Have you been fantasizing about writing a great book? Or you&apos;ve written, and all you get are rejections? The Craft of Creative Writing is the writing companion you need.
-                  No matter what your writing goals are, this book ensures nothing stands between you and good writing. It is a handbook. A guide. A reference point. A promise of hope for your writing. Exactly what you need. Writing is work, so let&apos;s get to work, shall
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative">
-            <div className="relative overflow-hidden bg-white shadow-md rounded-xl h-full">
-            <img src="/verahprofile.jpeg" alt="Image 1" className="h-70 w-full object-cover rounded-xl" />
-              <div className="p-4">
-                <h3 className="mt-2 text-2xl font-bold text-gray-900 sm:mt-10">The Craft of Creative Writing with Mugeni Ojiambo Ksh. 1,000</h3>
-                <p className="mt-2 text-base text-gray-600">
-                  Have you been fantasizing about writing a great book? Or you&apos;ve written, and all you get are rejections? The Craft of Creative Writing is the writing companion you need.
-                  No matter what your writing goals are, this book ensures nothing stands between you and good writing. It is a handbook. A guide. A reference point. A promise of hope for your writing. Exactly what you need. Writing is work, so let&apos;s get to work, shall
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative">
-            <div className="relative overflow-hidden bg-white shadow-md rounded-xl h-full">
-            <img src="/verahprofile.jpeg" alt="Image 1" className="h-70 w-full object-cover rounded-xl" />
-              <div className="p-4">
-                <h3 className="mt-2 text-2xl font-bold text-gray-900 sm:mt-10">The Craft of Creative Writing with Mugeni Ojiambo Ksh. 1,000</h3>
-                <p className="mt-2 text-base text-gray-600">
-                  Have you been fantasizing about writing a great book? Or you&apos;ve written, and all you get are rejections? The Craft of Creative Writing is the writing companion you need.
-                  No matter what your writing goals are, this book ensures nothing stands between you and good writing. It is a handbook. A guide. A reference point. A promise of hope for your writing. Exactly what you need. Writing is work, so let&apos;s get to work, shall
-                </p>
-              </div>
-            </div>
-          </div>
-
+          {books.map((book, index) => (
+            <Book key={index} {...book} />
+          ))}
         </div>
       </div>
     </section>
   );
-}
+};
 
 export default Books;
+
 
