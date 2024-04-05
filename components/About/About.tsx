14cc3cdd626961ce9c1f4679f6eb1758b39cf2f7
realgitau <antonygitau04@gmail.com>
2024-04-05 19:53:18 +0300

    nineth commit

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,38 +1,31 @@
 import React from 'react';
 
-const About = () => {
+const Write = () => {
   return (
-    <div id="about" className="relative bg-white overflow-hidden mt-16">
-    <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-            <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-                fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-                <polygon points="50,0 100,0 50,100 0,100"></polygon>
-            </svg>
-
-            <div className="pt-1"></div>
-
-            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-                <div className="sm:text-center lg:text-left ">
-                    <h2 className="my-6 text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
-                        About me
-                    </h2>
-
-                    <p>
-                    Verah Omwocha is a seasoned publishing professional with over a decade of experience in managing and maintaining high publication standards. Skilled in communications, book editing, and proofreading across various content types, she excels in providing strategic leadership to publication teams. Her expertise includes elevating the quality of institutional documents, website content, and stylistic editing initiatives.
-                    Verah is also proficient in conducting editorial training sessions and judging writing competitions. With a commitment to excellence, she contributes to enhancing readability, engagement, and brand visibility through her meticulous editing and leadership skills.
-                    She holds a Master's Degree in Literature from Kenyatta University and a Bachelor of Arts degree in Education
-                    (Language and Literature) from the University of Nairobi.
-                    She is married and has two daughters.
-
-                    </p>
-                </div>
-            </main>
+    <section className="bg-gray-100 py-12 rounded-lg">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto ">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-gray-800 uppercase mb-8">About</h2>
+          </div>
+          <div className="text-gray-700 text-lg leading-relaxed">
+            <p>
+              Verah Omwocha is a seasoned publishing professional with over a decade of experience in managing and maintaining high publication standards. Skilled in communications, book editing, and proofreading across various content types, she excels in providing strategic leadership to publication teams. Her expertise includes elevating the quality of institutional documents, website content, and stylistic editing initiatives.
+            </p>
+            <p className="mt-4">
+              Verah is also proficient in conducting editorial training sessions and judging writing competitions. With a commitment to excellence, she contributes to enhancing readability, engagement, and brand visibility through her meticulous editing and leadership skills.
+            </p>
+            <p className="mt-4">
+              She holds a Master's Degree in Literature from Kenyatta University and a Bachelor of Arts degree in Education (Language and Literature) from the University of Nairobi. She is married and has two daughters.
+            </p>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
+    </section>
   );
-}
+};
 
-export default About;
+export default Write;
+
+
 
