75c32fb72e1c0b133814a5399be259475b3cd658
realgitau <antonygitau04@gmail.com>
2024-04-05 21:47:25 +0300

    17th commit

--- components/Books/Books.tsx
+++ components/Books/Books.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import React from 'react';
 import Image from 'next/image';
@@ -34,33 +34,23 @@ const Books = () => {
       price: "Ksh. 950, AVAILABLE AT Nuria store.",
       description: "This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband's. It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It's about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks."
     },
+    {
+      title: "Diary of the Miaha Memoir",
+      imageSrc: "/verahprofile.jpeg",
+      price: "Ksh. 950, AVAILABLE AT Nuria store.",
+      description: "This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband's. It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It's about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks."
+    },
+    {
+      title: "Diary of the Miaha Memoir",
+      imageSrc: "/verahprofile.jpeg",
+      price: "Ksh. 950, AVAILABLE AT Nuria store.",
+      description: "This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband's. It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It's about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks."
+    },
     // Add more book entries here
   ];
 
   return (
     <section className="py-12 bg-white text-black sm:py-12 lg:py-16 p-10">
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="max-w-xl mx-auto text-center xl:max-w-2xl">
-          <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl xl:text-5xl mb-6">BOOKS </h2>
-          <p className="mb-4">Here are some of the Awards that I&apos;ve received over the years</p>
-        </div>
-        <div className="grid max-w-4xl lg:max-w-6xl grid-cols-1 mx-auto mt-8 text-center gap-y-4 sm:gap-x-8 sm:grid-cols-2 lg:grid-cols-3 sm:text-left">
-          {books.map((book, index) => (
-            <Book key={index} {...book} />
-          ))}
-        </div>
-      </div>
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="max-w-xl mx-auto text-center xl:max-w-2xl">
-          <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl xl:text-5xl mb-6">BOOKS </h2>
-          <p className="mb-4">Here are some of the Awards that I&apos;ve received over the years</p>
-        </div>
-        <div className="grid max-w-4xl lg:max-w-6xl grid-cols-1 mx-auto mt-8 text-center gap-y-4 sm:gap-x-8 sm:grid-cols-2 lg:grid-cols-3 sm:text-left">
-          {books.map((book, index) => (
-            <Book key={index} {...book} />
-          ))}
-        </div>
-      </div>
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="max-w-xl mx-auto text-center xl:max-w-2xl">
           <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl xl:text-5xl mb-6">BOOKS </h2>
