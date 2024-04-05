ec3626f642dc09112402cc3deb898714f6f38beb
realgitau <antonygitau04@gmail.com>
2024-04-05 11:39:19 +0300

    third commit

--- components/Books/Books.tsx
+++ components/Books/Books.tsx
@@ -1,56 +1,39 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { BsArrowUpRightSquare } from 'react-icons/bs'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { BsArrowUpRightSquare } from 'react-icons/bs';
 
 const Books = () => {
   return (
-    <section id='projects'>
-      <h1 className='text-center font-bold text-4xl'>My Books
-        <hr className='w-6 h-1 mx-auto bg-black border-0 rounded' />
-      </h1>
-      <div>
-        <div className='flex flex-col md:flex-row md:space-x-12'>
-          <div className='md:1/2'>
-            <Image src='/verahprofile.jpeg' alt='Vera' width={200} height={200} className='rounded-lg'/>
-          </div>
-          <div className='md:1/2'>
-            <h1 className='text-4xl font-bold mb-6'>Project Thoughts</h1>
-            <p className='text-xl leading-7 text-neutral-600 dark:text-neutral-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate saepe dignissimos laboriosam quod fugit. Ducimus?</p>
-          </div>
-          <div className='md:1/2'>
-            <Image src='/verahprofile.jpeg' alt='Vera' width={200} height={200} className='rounded-lg'/>
-          </div>
-          <div className='md:1/2'>
-            <h1 className='text-4xl font-bold mb-6'>Project Thoughts</h1>
-            <p className='text-xl leading-7 text-neutral-600 dark:text-neutral-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate saepe dignissimos laboriosam quod fugit. Ducimus?</p>
-          </div>
-          <div className='md:1/2'>
-            <Image src='/verahprofile.jpeg' alt='Vera' width={200} height={200} className='rounded-lg'/>
-          </div>
-          <div className='md:1/2'>
-            <h1 className='text-4xl font-bold mb-6'>Project Thoughts</h1>
-            <p className='text-xl leading-7 text-neutral-600 dark:text-neutral-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate saepe dignissimos laboriosam quod fugit. Ducimus?</p>
-          </div>
-          <div className='md:1/2'>
-            <Image src='/verahprofile.jpeg' alt='Vera' width={200} height={200} className='rounded-lg'/>
-          </div>
-          <div className='md:1/2'>
-            <h1 className='text-4xl font-bold mb-6'>Project Thoughts</h1>
-            <p className='text-xl leading-7 text-neutral-600 dark:text-neutral-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate saepe dignissimos laboriosam quod fugit. Ducimus?</p>
-          </div>
-          <div className='md:1/2'>
-            <Image src='/verahprofile.jpeg' alt='Vera' width={200} height={200} className='rounded-lg'/>
-          </div>
-          <div className='md:1/2'>
-            <h1 className='text-4xl font-bold mb-6'>Project Thoughts</h1>
-            <p className='text-xl leading-7 text-neutral-600 dark:text-neutral-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate saepe dignissimos laboriosam quod fugit. Ducimus?</p>
-          </div>
-        </div>
+    <section id='projects' className="py-12">
+      <h1 className='text-center font-bold text-4xl mb-8'>My Books</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
+        <BookItem />
       </div>
-      
     </section>
-  )
-}
+  );
+};
 
-export default Books
+const BookItem = () => {
+  return (
+    <div className="flex flex-col bg-white shadow-md rounded-lg p-6">
+      <div className="flex justify-center mb-6">
+        <Image src='/verahprofile.jpeg' alt='Vera' width={200} height={200} className='rounded-lg' />
+      </div>
+      <h1 className='text-2xl font-bold mb-4'>Project Thoughts</h1>
+      <p className='text-lg leading-7 text-gray-600 dark:text-gray-400 mb-6'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate saepe dignissimos laboriosam quod fugit. Ducimus?</p>
+      <div className="flex justify-end">
+        <Link href="#"className="text-blue-500 hover:underline flex items-center">
+            Read More <BsArrowUpRightSquare className="ml-1" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Books;
--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="max-w-lg mx-auto mt-8 px-4">
+      <h2 className="text-3xl font-semibold mb-6 text-center">Contact Us</h2>
+      <form className="space-y-6">
+        <div>
+          <label htmlFor="name" className="block mb-1">Name:</label>
+          <input type="text" id="name" name="name" className="w-full border border-gray-300 rounded-md p-3 focus:outline-none focus:border-blue-500" />
+        </div>
+        <div>
+          <label htmlFor="email" className="block mb-1">Email:</label>
+          <input type="email" id="email" name="email" className="w-full border border-gray-300 rounded-md p-3 focus:outline-none focus:border-blue-500" />
+        </div>
+        <div>
+          <label htmlFor="phone" className="block mb-1">Phone Number:</label>
+          <input type="tel" id="phone" name="phone" className="w-full border border-gray-300 rounded-md p-3 focus:outline-none focus:border-blue-500" />
+        </div>
+        <div>
+          <label htmlFor="message" className="block mb-1">Message:</label>
+          <textarea id="message" name="message" className="w-full border border-gray-300 rounded-md p-3 focus:outline-none focus:border-blue-500 h-40 resize-none"></textarea>
+        </div>
+        <button type="submit" className="bg-blue-500 text-white py-3 px-6 rounded-md hover:bg-blue-600 w-full">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
