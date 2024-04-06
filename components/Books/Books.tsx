c142f4a8a838907ed0f44f47352733ae1a37cdde
realgitau <antonygitau04@gmail.com>
2024-04-06 09:51:50 +0300

    24th commit

--- components/Books/Books.tsx
+++ components/Books/Books.tsx
@@ -1,32 +1,44 @@
 "use client"
 
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 interface BookProps {
   title: string;
   imageSrc: string;
   price: string;
-  description: string;
+  description?: string; // Making description optional
 }
 
-// A component for displaying individual book information
-const Book: React.FC<BookProps> = ({ title, imageSrc, price, description }) => {
+const Book: React.FC<BookProps> = ({ title, imageSrc, price, description = '' }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+  const truncatedDescription = description.slice(0, 50);
+
+  const toggleDescription = () => {
+    setShowFullDescription(!showFullDescription);
+  };
+
   return (
-    <div className="relative overflow-hidden w-full  bg-white shadow-md rounded-xl">
+    <div className="relative overflow-hidden w-full bg-white shadow-md rounded-xl">
       <div className="h-80 md:h-96 w-full relative">
         <Image src={imageSrc} alt={title} layout="fill" objectFit="cover" className="rounded-xl" />
       </div>
       <div className="p-4">
         <h3 className="text-xl md:text-2xl font-bold text-gray-900">{title}</h3>
-        <p className="mt-2 text-base text-gray-600">{description}</p>
+        <p className="mt-2 text-base text-gray-600">
+          {showFullDescription ? description : `${truncatedDescription}...`}
+          <button className="text-blue-500 ml-2" onClick={toggleDescription}>
+            {showFullDescription ? "Read Less" : "Read More"}
+          </button>
+        </p>
         <p className="mt-2 text-base text-gray-600">Price: {price}</p>
+        <button className="mt-4 bg-black text-white py-2 px-4 rounded hover:bg-white hover:text-black">Buy</button>
       </div>
     </div>
   );
 };
 
-const Books = () => {
+const Books: React.FC = () => {
   const books: BookProps[] = [
     {
       title: "Diary of the Miaha Memoir",
@@ -34,23 +46,29 @@ const Books = () => {
       price: "Ksh. 950, AVAILABLE AT Nuria store.",
       description: "This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband&apos;s. It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It&apos;s about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks."
     },
+    {
+      title: "Diary of the Miaha Memoir",
+      imageSrc: "/verahprofile.jpeg",
+      price: "Ksh. 950, AVAILABLE AT Nuria store.",
+      description: "This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband&apos;s. It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It&apos;s about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks."
+    },
+    {
+      title: "Diary of the Miaha Memoir",
+      imageSrc: "/verahprofile.jpeg",
+      price: "Ksh. 950, AVAILABLE AT Nuria store.",
+      description: "This is a story of a young girl lost in a world too big for her petite body and unwelcome ideas. A girl trying to understand the love of God. And her husband&apos;s. It is about a girl, who is trying to fix puzzles to understand why she is here. And to do what she was sent to do. It&apos;s about a girl on a journey to finding herself. To healing the child in her. To finding peace. And digging deep into her soul, to find forgiveness and hand it to the world. Is marriage welcoming of a young girl, broken in so many ways? She asks."
+    },
+    // Add more books if needed
   ];
 
   return (
-    <section className="py-12 bg-white text-black sm:py-12 lg:py-16 p-10">
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="max-w-xl mx-auto text-center xl:max-w-2xl">
-          <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl xl:text-5xl mb-6">BOOKS</h2>
-          <p className="mb-4">Here are some of the Awards that I&apos;ve received over the years</p>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {books.map((book, index) => (
-            <Book key={index} {...book} />
-          ))}
-        </div>
-      </div>
-    </section>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {books.map((book, index) => (
+        <Book key={index} {...book} />
+      ))}
+    </div>
   );
 };
 
 export default Books;
+
