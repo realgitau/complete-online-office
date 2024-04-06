d25610ef7364a270afd44fdb1fb0f7b98304ca5e
realgitau <antonygitau04@gmail.com>
2024-04-06 10:24:14 +0300

    25th commit

--- components/Awards/Awards.tsx
+++ components/Awards/Awards.tsx
@@ -2,50 +2,43 @@ import React from 'react';
 
 const Awards = () => {
   return (
-    <section className="py-12 bg-gray-900 text-gray-100 sm:py-12 lg:py-16 p-10 rounded-xl">
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="max-w-xl mx-auto text-center xl:max-w-2xl">
-          <h2 className="text-3xl font-bold leading-tight text-gray-50 sm:text-4xl xl:text-5xl mb-6">AWARDS!</h2>
-          <p className="mb-4">Here are some of the Awards that I&apos;ve received over the years</p>
-        </div>
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-          <div className="relative overflow-hidden bg-white shadow-md rounded-xl">
-            <div className="p-9">
-              <h3 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-10">Publications</h3>
-              <div className="mt-6 text-base text-gray-600">
-                <ul>
-                  {/* List items */}
-                </ul>
-                <a href="" className="block mt-2 underline">Visit: https://linktr.ee/verahomwocha to read samples</a>
-              </div>
-            </div>
-          </div>
+    <section id="features" className="container bg-black mx-auto px-4 space-y-6 bg-black-50 py-8 md:py-12 lg:py-20 rounded-xl">
+      <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+        <h2 className="font-bold text-3xl text-white leading-[1.1] sm:text-3xl md:text-6xl">Services</h2>
+        <p className="max-w-[85%] leading-normal text-white text-muted-foreground sm:text-lg sm:leading-7">
+          Here is a list of some of the additional services I offer.
+        </p>
+      </div>
 
-          <div className="relative overflow-hidden bg-white shadow-md rounded-xl">
-            <div className="p-9">
-              <h3 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-10">Publications</h3>
-              <div className="mt-6 text-base text-gray-600">
-                <ul>
-                  {/* List items */}
-                </ul>
-                <a href="" className="block mt-2 underline">Visit: https://linktr.ee/verahomwocha to read samples</a>
-              </div>
-            </div>
-          </div>
+      <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
 
-          <div className="relative overflow-hidden bg-white shadow-md rounded-xl">
-            <div className="p-9">
-              <h3 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-10">Publications</h3>
-              <div className="mt-6 text-base text-gray-600">
-                <ul>
-                  {/* List items */}
-                </ul>
-                <a href="" className="block mt-2 underline">Visit: https://linktr.ee/verahomwocha to read samples</a>
-              </div>
+        <div className="relative overflow-hidden rounded-lg border bg-white dark:bg-black select-none hover:shadow hover:shadow-teal-200 p-2">
+          <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+            <div className="space-y-2 dark:text-white">
+              <h3 className="font-bold dark:text-white">Consultation</h3>
+              <p className="text-sm text-muted-foreground">App dir, Routing, Layouts, Loading UI and API routes.</p>
             </div>
           </div>
-          {/* Repeat the above div structure for other awards */}
         </div>
+
+        <div className="relative overflow-hidden rounded-lg border bg-white dark:bg-black select-none hover:shadow hover:shadow-teal-200 p-2">
+          <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+            <div className="space-y-2 dark:text-white">
+              <h3 className="font-bold dark:text-white">Consultation</h3>
+              <p className="text-sm text-muted-foreground">App dir, Routing, Layouts, Loading UI and API routes.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative overflow-hidden rounded-lg border bg-white dark:bg-black select-none hover:shadow hover:shadow-teal-200 p-2">
+          <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+            <div className="space-y-2 dark:text-white">
+              <h3 className="font-bold dark:text-white">Consultation</h3>
+              <p className="text-sm text-muted-foreground">App dir, Routing, Layouts, Loading UI and API routes.</p>
+            </div>
+          </div>
+        </div>
+
       </div>
     </section>
   );
